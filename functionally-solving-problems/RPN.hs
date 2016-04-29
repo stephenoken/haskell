58a279c04393fe1f25b04b69aa815785c4df0d35
Stephen O'Kennedy <stephenoken@gmail.com>
2016-04-29 14:26:57 +0100

    "Added a RPN calculator"
    ""

---
+++ functionally-solving-problems/RPN.hs
@@ -0,0 +1,13 @@
+import Data.List
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+  where
+    foldingFunction (x:y:xs) "*" = (x*y):xs
+    foldingFunction (x:y:xs) "/" = (y/x):xs
+    foldingFunction (x:y:xs) "+" = (x+y):xs
+    foldingFunction (x:y:xs) "-" = (y-x):xs
+    foldingFunction (x:y:xs) "^" = (y**x):xs
+    foldingFunction (x:xs) "ln" = log x:xs
+    foldingFunction xs "sum" = [sum xs]
+    foldingFunction xs numberString = read numberString:xs
